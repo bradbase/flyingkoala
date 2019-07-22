5d2217363a4af5a294c0573cd1f61d17018fe49d
Bradley van Ree <47906258+bradbase@users.noreply.github.com>
2019-07-22 22:50:36 +1000

    Updated readthedocs to remove the Contents caption and remove unused libraries.

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '0.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.autodoc'
+extensions = ['recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
