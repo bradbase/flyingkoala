7ec4b2ab97fc35719a388e68fe427c7e772fd07d
Bradley van Ree <47906258+bradbase@users.noreply.github.com>
2019-07-31 00:02:51 +1000

    Bugfix add-in

--- flyingkoala/timeseries/__init__.py
+++ flyingkoala/timeseries/__init__.py
@@ -3,4 +3,3 @@ from .timeseries import KEEPRECORDS
 from .timeseries import RESAMPLEMINS
 from .timeseries import RESAMPLEMINSWITHINDEX
 from .timeseries import TIMEISBETWEEN
-from .timeseries import FILTERED
