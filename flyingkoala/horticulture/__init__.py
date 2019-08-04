92c0b1d1c157115326b6d53ef20503111a81f306
Bradley van Ree <47906258+bradbase@users.noreply.github.com>
2019-08-05 08:20:23 +1000

    docs bugfix

--- flyingkoala/horticulture/__init__.py
+++ flyingkoala/horticulture/__init__.py
@@ -2,5 +2,3 @@ from .horticulture import DegreeDay
 from .horticulture import DegreeDayDynamicArray
 from .horticulture import DegreeDayByName
 from .horticulture import DegreeDayDynamicArrayByName
-from .horticulture import DegreeDayByNamedRange
-from .horticulture import DegreeDayDynamicArrayByNamedRange
