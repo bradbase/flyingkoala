e107c5b3ceeca12b129fce0d43e190977bbdc157
Bradley van Ree <47906258+bradbase@users.noreply.github.com>
2019-08-04 23:44:11 +1000

    Attempting to get appidoc working at readthedocs (it works locally)

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-if not 'READTHEDOCS' in os.environ:
-    sys.path.insert(0, os.path.abspath('..'))
+# if not 'READTHEDOCS' in os.environ:
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath('./flyingkoala/'))
 
 master_doc = 'index'
