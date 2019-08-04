fb966886d2875efc1fd2a343970a8bbf905e34b5
Bradley van Ree <47906258+bradbase@users.noreply.github.com>
2019-08-05 07:12:49 +1000

    autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,8 @@ import sphinx
 
 # if not 'READTHEDOCS' in os.environ:
 # sys.path.insert(0, os.path.abspath('..'))
-# sys.path.append(os.path.abspath('./flyingkoala/'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('../flyingkoala/'))
 
 master_doc = 'index'
 
