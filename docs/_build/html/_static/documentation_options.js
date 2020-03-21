5af9ec6864e4f301ed18d6e441e1ebd661577753
Bradley van Ree <47906258+bradbase@users.noreply.github.com>
2020-03-21 13:55:10 +1100

    Updating readthedocs config
    Co-Authored-By: Bradley van Ree <bradbase@users.noreply.github.com>

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.3b',
+    VERSION: '0.0.5b0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
